79d0ac69cfb7a3c4aa52ef494d8d89585a89c957
ALESSIO CARACHINO s296138 <alessio.carachino.g@gmail.com>
2022-05-03 09:23:37 +0200

    added report doc

--- functions/mlFunc.py
+++ functions/mlFunc.py
@@ -5,15 +5,18 @@ import pylab
 import scipy.linalg
 import sklearn.datasets
 
+
 def mcol(v):
     return v.reshape((v.size, 1))
+
+
 def mrow(v):
     return v.reshape((1, v.size))
+
 
 def load(fname):
     DList = []
     labelsList = []
-    
 
     with open(fname) as f:
         try:
@@ -27,19 +30,21 @@ def load(fname):
         except:
             pass
     return numpy.hstack(DList), numpy.array(labelsList, dtype=numpy.int32)
-    
+
+
 def emprical_mean(D):
     return mcol(D.mean(1))
+
 
 def empirical_covariance(D, mu):
     n = numpy.shape(D)[1]
     DC = D - mcol(mu)
-    C = 1/n * numpy.dot(DC, numpy.transpose(DC))
+    C = 1 / n * numpy.dot(DC, numpy.transpose(DC))
     return C
 
-def PCA(D, L):
 
-# 1. compute covariance matrix
+def PCA(D, L):
+    # 1. compute covariance matrix
     n = numpy.shape(D)[1]
     # mu = dataset mean, calculated by axis = 1 (columns mean)
     # the result is an array of means for each column
@@ -49,10 +54,10 @@ def PCA(D, L):
     # so I can center the data
     DC = D - mcol(mu)
 
-    #calculate covariance matrix with DataCentered matrix
-    C = 1/n * numpy.dot(DC, numpy.transpose(DC))
+    # calculate covariance matrix with DataCentered matrix
+    C = 1 / n * numpy.dot(DC, numpy.transpose(DC))
 
-    #Calculate eigenvectors and eigenvalues of C with singular value decomposition
+    # Calculate eigenvectors and eigenvalues of C with singular value decomposition
     # That's why C is semi-definite positive, so we can get the sorted eigenvectors
     # from the svd: C=U*(Sigma)*V(^transposed)
     # svd() returns sorted eigenvalues from smallest to largest,
@@ -63,7 +68,7 @@ def PCA(D, L):
     m = 2
     P = USVD[:, 0:m]
 
-    #apply the projection to the matrix of samples D
+    # apply the projection to the matrix of samples D
     DP = numpy.dot(P.T, D)
     print(DP)
     hlabels = {
@@ -72,173 +77,177 @@ def PCA(D, L):
     }
 
     for i in range(2):
-#I have to invert the sign of the second eigenvector to flip the image
-        plt.scatter(DP[:, L==i][0], -DP[:, L==i][1], label = hlabels.get(i))
+        # I have to invert the sign of the second eigenvector to flip the image
+        plt.scatter(DP[:, L == i][0], -DP[:, L == i][1], label=hlabels.get(i))
         plt.legend()
         plt.tight_layout()
     plt.show()
 
+
 def ML_GAU(D):
     m = emprical_mean(D)
-    C= empirical_covariance(D, m)
+    C = empirical_covariance(D, m)
     return m, C
 
+
 def logpdf_GAU_ND(X, mu, C):
-    P  = numpy.linalg.inv(C)
-    const = -0.5 * X.shape[0] * numpy.log(2*numpy.pi)
+    P = numpy.linalg.inv(C)
+    const = -0.5 * X.shape[0] * numpy.log(2 * numpy.pi)
     const += -0.5 * numpy.linalg.slogdet(C)[1]
 
     Y = []
 
     for i in range(X.shape[1]):
-        x = X[:, i:i+1]
-        res = const + -0.5 * numpy.dot((x-mu).T, numpy.dot(P, (x-mu)))
+        x = X[:, i:i + 1]
+        res = const + -0.5 * numpy.dot((x - mu).T, numpy.dot(P, (x - mu)))
         Y.append(res)
     return numpy.array(Y).ravel()
+
 
 def loglikelihood(XND, m_ML, C_ML):
     return logpdf_GAU_ND(XND, m_ML, C_ML).sum()
 
+
 def likelihood(XND, m_ML, C_ML):
     return numpy.exp(loglikelihood(XND, m_ML, C_ML))
 
+
 def MGC(DTE, LTE, DTR, LTR):
     h = {}
-    
+
     for i in range(2):
-        mu, C = ML_GAU(DTR[:, LTR==i])
+        mu, C = ML_GAU(DTR[:, LTR == i])
         h[i] = (mu, C)
-        
+
     SJoint = numpy.zeros((2, DTE.shape[1]))
     logSJoint = numpy.zeros((2, DTE.shape[1]))
     classPriors = [0.5, 0.5]
-    
+
     for label in range(2):
         mu, C = h[label]
-        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, C).ravel()) * classPriors[label] 
+        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, C).ravel()) * classPriors[label]
         logSJoint[label, :] = logpdf_GAU_ND(DTE, mu, C).ravel() + numpy.log(classPriors[label])
-        
-    
+
     SMarginal = SJoint.sum(0)
     logSMarginal = scipy.special.logsumexp(logSJoint, axis=0)
-    
+
     Post1 = SJoint / mrow(SMarginal)
     logPost = logSJoint - mrow(logSMarginal)
     Post2 = numpy.exp(logPost)
-    
+
     LPred1 = Post1.argmax(0)
     LPred2 = Post2.argmax(0)
     return LPred1, LPred2
 
+
 def test(LTE, LPred):
-    accuracy = (LTE == LPred).sum() /LTE.size
+    accuracy = (LTE == LPred).sum() / LTE.size
     error = 1 - accuracy
     return accuracy, error
 
+
 def naive_MGC(DTE, LTE, DTR, LTR):
-    
     h = {}
-    
+
     for i in range(2):
-        mu, C = ML_GAU(DTR[:, LTR==i])
+        mu, C = ML_GAU(DTR[:, LTR == i])
         C = C * numpy.identity(C.shape[0])
         h[i] = (mu, C)
-        
+
     SJoint = numpy.zeros((2, DTE.shape[1]))
     logSJoint = numpy.zeros((2, DTE.shape[1]))
     classPriors = [0.5, 0.5]
-    
+
     for label in range(2):
         mu, C = h[label]
-        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, C).ravel()) * classPriors[label] 
+        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, C).ravel()) * classPriors[label]
         logSJoint[label, :] = logpdf_GAU_ND(DTE, mu, C).ravel() + numpy.log(classPriors[label])
-        
-    
+
     SMarginal = SJoint.sum(0)
     logSMarginal = scipy.special.logsumexp(logSJoint, axis=0)
-    
+
     Post1 = SJoint / mrow(SMarginal)
     logPost = logSJoint - mrow(logSMarginal)
     Post2 = numpy.exp(logPost)
-    
+
     LPred1 = Post1.argmax(0)
     LPred2 = Post2.argmax(0)
     return LPred1, LPred2
-    
+
+
 def tied_cov_GC(DTE, LTE, DTR, LTR):
     h = {}
     Ctot = 0
     for i in range(2):
-        mu, C = ML_GAU(DTR[:, LTR==i])
-        Ctot += DTR[:, LTR==i].shape[1] * C
+        mu, C = ML_GAU(DTR[:, LTR == i])
+        Ctot += DTR[:, LTR == i].shape[1] * C
         h[i] = (mu)
-        
+
     Ctot = Ctot / DTR.shape[1]
-    
-        
-    
+
     SJoint = numpy.zeros((2, DTE.shape[1]))
     logSJoint = numpy.zeros((2, DTE.shape[1]))
     classPriors = [0.5, 0.5]
-    
+
     for label in range(2):
         mu = h[label]
-        
-        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, Ctot).ravel()) * classPriors[label] 
+
+        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, Ctot).ravel()) * classPriors[label]
         logSJoint[label, :] = logpdf_GAU_ND(DTE, mu, Ctot).ravel() + numpy.log(classPriors[label])
-        
-    
+
     SMarginal = SJoint.sum(0)
     logSMarginal = scipy.special.logsumexp(logSJoint, axis=0)
-    
+
     Post1 = SJoint / mrow(SMarginal)
     logPost = logSJoint - mrow(logSMarginal)
     Post2 = numpy.exp(logPost)
-    
+
     LPred1 = Post1.argmax(0)
     LPred2 = Post2.argmax(0)
     return LPred1, LPred2
+
 
 def tied_cov_naive_GC(DTE, LTE, DTR, LTR):
     h = {}
     Ctot = 0
     for i in range(2):
-        mu, C = ML_GAU(DTR[:, LTR==i])
-        Ctot += DTR[:, LTR==i].shape[1] * C
+        mu, C = ML_GAU(DTR[:, LTR == i])
+        Ctot += DTR[:, LTR == i].shape[1] * C
         h[i] = (mu)
-        
+
     Ctot = Ctot / DTR.shape[1]
     Ctot = Ctot * numpy.identity(Ctot.shape[0])
-        
+
     SJoint = numpy.zeros((2, DTE.shape[1]))
     logSJoint = numpy.zeros((2, DTE.shape[1]))
     classPriors = [0.5, 0.5]
-    
+
     for label in range(2):
         mu = h[label]
-        
-        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, Ctot).ravel()) * classPriors[label] 
+
+        SJoint[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, Ctot).ravel()) * classPriors[label]
         logSJoint[label, :] = logpdf_GAU_ND(DTE, mu, Ctot).ravel() + numpy.log(classPriors[label])
-        
-    
+
     SMarginal = SJoint.sum(0)
     logSMarginal = scipy.special.logsumexp(logSJoint, axis=0)
-    
+
     Post1 = SJoint / mrow(SMarginal)
     logPost = logSJoint - mrow(logSMarginal)
     Post2 = numpy.exp(logPost)
-    
+
     LPred1 = Post1.argmax(0)
     LPred2 = Post2.argmax(0)
     return LPred1, LPred2
-    
+
+
 def plot():
     plt.figure()
-    XPlot = numpy.linspace(-8, 12, 1000) 
-    m = numpy.ones((1,1)) * 1.0
-    C = numpy.ones((1,1)) * 2.0
-    plt.plot(XPlot.ravel(), numpy.exp(logpdf_GAU_ND(mrow(XPlot), m, C))) 
+    XPlot = numpy.linspace(-8, 12, 1000)
+    m = numpy.ones((1, 1)) * 1.0
+    C = numpy.ones((1, 1)) * 2.0
+    plt.plot(XPlot.ravel(), numpy.exp(logpdf_GAU_ND(mrow(XPlot), m, C)))
     plt.show()
+
 
 def plot_hist_exp(X1D, m_ML, C_ML):
     plt.figure()
@@ -247,19 +256,19 @@ def plot_hist_exp(X1D, m_ML, C_ML):
     plt.plot(XPlot.ravel(), numpy.exp(logpdf_GAU_ND(mrow(XPlot), m_ML, C_ML)))
     plt.show()
 
-# PLOTTING FUNCTIONS 
-def plot_hist(D, L):
 
-    D0 = D[:, L==0]
-    D1 = D[:, L==1]
+# PLOTTING FUNCTIONS
+def plot_hist(D, L):
+    D0 = D[:, L == 0]
+    D1 = D[:, L == 1]
 
     for dIdx in range(12):
         plt.figure()
-        
-        plt.hist(D0[dIdx, :], bins = 10, density = True, alpha = 0.4, label = 'male')
-        plt.hist(D1[dIdx, :], bins = 10, density = True, alpha = 0.4, label = 'female')
-        
+
+        plt.hist(D0[dIdx, :], bins=10, density=True, alpha=0.4, label='male')
+        plt.hist(D1[dIdx, :], bins=10, density=True, alpha=0.4, label='female')
+
         plt.legend()
-        plt.tight_layout() # Use with non-default font size to keep axis label inside the figure
+        plt.tight_layout()  # Use with non-default font size to keep axis label inside the figure
         plt.savefig('hist_%d.pdf' % dIdx)
     plt.show()
--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import pylab
 import scipy.linalg
 import sklearn.datasets
 import sys
+
 sys.path.append("./functions")
 from mlFunc import *
 
 if __name__ == "__main__":
     DTR, LTR = load("Train.txt")
     DTE, LTE = load("Test.txt")
-    #plot_hist(D, L)
-    
-    
-    #We're starting with Multivariate Gaussian Classifier
+    # plot_hist(D, L)
+
+    # We're starting with Multivariate Gaussian Classifier
     _, LPred2 = MGC(DTE, LTE, DTR, LTR)
     _, LP2n = naive_MGC(DTE, LTE, DTR, LTR)
     _, LP2t = tied_cov_GC(DTE, LTE, DTR, LTR)
@@ -24,8 +24,13 @@ if __name__ == "__main__":
     log_acc_n, log_err_n = test(LTE, LP2n)
     log_acc_t, log_err_t = test(LTE, LP2t)
     log_acc_nt, log_err_nt = test(LTE, LP2nt)
-    
+
+    print(log_acc)
+    print(log_acc_n)
+    print(log_acc_t)
+    print(log_acc_nt)
+
     # Notiamo che le features sono molto correlate tra loro,
     # quindi non possiamo fare l'assunzione di indipendenza di Naive Bayes
-    
-    #PCA(D, L)
+
+    # PCA(D, L)
