7b518dfbe858f8852858774272fa1b0d6831ffa2
Peipi98 <atanasio.peppe@gmail.com>
2022-05-07 13:31:52 +0200

    added kfold implementation
    Co-Authored-By: CarachinoAlessio <73645335+CarachinoAlessio@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,13 +4,63 @@ import matplotlib.pyplot as plt
 import pylab
 import scipy.linalg
 import sklearn.datasets
-import sys
+#import sys
 
-sys.path.append("./functions")
+#sys.path.append("./functions")
 from mlFunc import *
 
+def kfold_cross(func, DTR, LTR, k):
+    accuracy = []
+    Dtr = numpy.split(DTR, k, axis=1)
+    Ltr = numpy.split(LTR, k)
+    
+    for i in range(k):
+        D = []
+        L = []
+        if i == 0 or i == k-1: 
+            continue
+        D.append(numpy.hstack(Dtr[:i]))
+        D.append(numpy.hstack(Dtr[i+1:]))
+        D = numpy.hstack(D)
+        
+        L.append(numpy.hstack(Ltr[:i]))
+        L.append(numpy.hstack(Ltr[i+1:]))
+        L = numpy.hstack(L)
+        
+        DTE = Dtr[i]
+        LTE = Ltr[i]
+        #print(str(DTE) + " " + str(i))
+        _, lpred  = func(DTE, LTE, D, L)
+        acc, _ = test(LTE, lpred)
+        accuracy.append(acc)
+        
+    return numpy.mean(accuracy)
+
+def leave_one_out(func, DTR, LTR):
+    accuracy = []
+    
+    for i in range(DTR.shape[1]):
+        D = []
+        L = []
+        D.append(DTR[:,:i])
+        D.append(DTR[:,i+1:])
+        D = numpy.hstack(D)
+        
+        L.append(LTR[:i])
+        L.append(LTR[i+1:])
+        L = numpy.hstack(L)
+        
+        DTE = DTR[:, i]
+        LTE = LTR[i]
+        #print(str(DTE) + " " + str(i))
+        _, lpred  = func(mcol(DTE), mcol(LTE), D, L)
+        acc, _ = test(LTE, lpred)
+        accuracy.append(acc)
+    return numpy.mean(accuracy)
+        
 if __name__ == "__main__":
     DTR, LTR = load("Train.txt")
+    
     DTE, LTE = load("Test.txt")
     # plot_hist(D, L)
 
@@ -24,12 +74,21 @@ if __name__ == "__main__":
     log_acc_n, log_err_n = test(LTE, LP2n)
     log_acc_t, log_err_t = test(LTE, LP2t)
     log_acc_nt, log_err_nt = test(LTE, LP2nt)
-
-    print(log_acc)
-    print(log_acc_n)
-    print(log_acc_t)
-    print(log_acc_nt)
-
+    
+    # print(leave_one_out(MGC, DTR, LTR))
+    # print(leave_one_out(naive_MGC, DTR, LTR))
+    # print(leave_one_out(tied_cov_GC, DTR, LTR))
+    # print(leave_one_out(tied_cov_naive_GC, DTR, LTR))
+    kfold_cross(MGC, DTR, LTR, 10)
+    
+    # DA CHIEDERE
+    # Notiamo che i risultati di leave-one-out sono rispettivamente
+    # più bassi rispetto ai precedenti non naive, ma più alti dei naive.
+    # 0.9753333333333334 
+    # 0.7031666666666667
+    # 0.9755
+    # 0.7048333333333333
+    
     # Notiamo che le features sono molto correlate tra loro,
     # quindi non possiamo fare l'assunzione di indipendenza di Naive Bayes
 
