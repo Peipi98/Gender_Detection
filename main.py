7914fb9f5fd1afa4cb9352f7df6c57577b3770d7
ALESSIO CARACHINO s296138 <alessio.carachino.g@gmail.com>
2022-05-08 11:44:39 +0200

    holdout technique + word formatting

--- main.py
+++ main.py
@@ -1,71 +1,82 @@
-import numpy
-import matplotlib
-import matplotlib.pyplot as plt
-import pylab
-import scipy.linalg
-import sklearn.datasets
-#import sys
-
-#sys.path.append("./functions")
 from mlFunc import *
+
 
 def kfold_cross(func, DTR, LTR, k):
     accuracy = []
     Dtr = numpy.split(DTR, k, axis=1)
     Ltr = numpy.split(LTR, k)
-    
+
     for i in range(k):
         D = []
         L = []
         if i == 0:
-            D.append(numpy.hstack(Dtr[i+1:]))
-            L.append(numpy.hstack(Ltr[i+1:]))
-        elif i == k-1:
+            D.append(numpy.hstack(Dtr[i + 1:]))
+            L.append(numpy.hstack(Ltr[i + 1:]))
+        elif i == k - 1:
             D.append(numpy.hstack(Dtr[:i]))
             L.append(numpy.hstack(Ltr[:i]))
         else:
             D.append(numpy.hstack(Dtr[:i]))
-            D.append(numpy.hstack(Dtr[i+1:]))
+            D.append(numpy.hstack(Dtr[i + 1:]))
             L.append(numpy.hstack(Ltr[:i]))
-            L.append(numpy.hstack(Ltr[i+1:]))
-            
+            L.append(numpy.hstack(Ltr[i + 1:]))
+
         D = numpy.hstack(D)
         L = numpy.hstack(L)
-        
+
         DTE = Dtr[i]
         LTE = Ltr[i]
-        #print(str(DTE) + " " + str(i))
-        _, lpred  = func(DTE, LTE, D, L)
+        # print(str(DTE) + " " + str(i))
+        _, lpred = func(DTE, LTE, D, L)
         acc, _ = test(LTE, lpred)
         accuracy.append(acc)
-        
+
     return numpy.mean(accuracy)
+
+
+def holdout_validation(func, D, L, seed=0, trainPerc=0.8):
+    nTrain = int(D.shape[1] * trainPerc)
+    numpy.random.seed(seed)
+    idx = numpy.random.permutation(D.shape[1])
+    idxTrain = idx[0:nTrain]
+    idxTest = idx[nTrain:]
+    DTR = D[:, idxTrain]
+    DTE = D[:, idxTest]
+    LTR = L[idxTrain]
+    LTE = L[idxTest]
+
+    _, lpred = func(DTE, LTE, DTR, LTR)
+    acc, _ = test(LTE, lpred)
+
+    return acc
+
 
 def leave_one_out(func, DTR, LTR):
     accuracy = []
-    
+
     for i in range(DTR.shape[1]):
         D = []
         L = []
-        D.append(DTR[:,:i])
-        D.append(DTR[:,i+1:])
+        D.append(DTR[:, :i])
+        D.append(DTR[:, i + 1:])
         D = numpy.hstack(D)
-        
+
         L.append(LTR[:i])
-        L.append(LTR[i+1:])
+        L.append(LTR[i + 1:])
         L = numpy.hstack(L)
-        
+
         DTE = DTR[:, i]
         LTE = LTR[i]
-        #print(str(DTE) + " " + str(i))
-        _, lpred  = func(mcol(DTE), mcol(LTE), D, L)
+        # print(str(DTE) + " " + str(i))
+        _, lpred = func(mcol(DTE), mcol(LTE), D, L)
         acc, _ = test(LTE, lpred)
         accuracy.append(acc)
     return numpy.mean(accuracy)
-        
+
+
 if __name__ == "__main__":
     DTR, LTR = load("Train.txt")
-    
+
     DTE, LTE = load("Test.txt")
     # plot_hist(D, L)
 
@@ -79,13 +90,22 @@ if __name__ == "__main__":
     log_acc_n, log_err_n = test(LTE, LP2n)
     log_acc_t, log_err_t = test(LTE, LP2t)
     log_acc_nt, log_err_nt = test(LTE, LP2nt)
-    
+
+    # print(holdout_validation(MGC, DTR, LTR))
+    # print(holdout_validation(naive_MGC, DTR, LTR))
+    # print(holdout_validation(tied_cov_GC, DTR, LTR))
+    # print(holdout_validation(tied_cov_naive_GC, DTR, LTR))
+    # 0.9683333333333334
+    # 0.71
+    # 0.9675
+    # 0.7125
+
     # print(leave_one_out(MGC, DTR, LTR))
     # print(leave_one_out(naive_MGC, DTR, LTR))
     # print(leave_one_out(tied_cov_GC, DTR, LTR))
     # print(leave_one_out(tied_cov_naive_GC, DTR, LTR))
     kfold_cross(MGC, DTR, LTR, 10)
-    
+
     # DA CHIEDERE
     # Notiamo che i risultati di leave-one-out sono rispettivamente
     # più bassi rispetto ai precedenti non naive, ma più alti dei naive.
@@ -93,7 +113,7 @@ if __name__ == "__main__":
     # 0.7031666666666667
     # 0.9755
     # 0.7048333333333333
-    
+
     # Notiamo che le features sono molto correlate tra loro,
     # quindi non possiamo fare l'assunzione di indipendenza di Naive Bayes
 
