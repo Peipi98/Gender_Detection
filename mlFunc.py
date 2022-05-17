70b39cde4ab753b78e4592c6851d1c0f023886bc
ALESSIO CARACHINO s296138 <alessio.carachino.g@gmail.com>
2022-05-17 08:57:39 +0200

    cleanup

--- mlFunc.py
+++ mlFunc.py
@@ -254,13 +254,13 @@ def logreg_obj_wrap(DTR, LTR, l):
 def to_be_transfered_into_main(DTR, LTR, DTE, LTE):
     for l in [1e-6, 1e-3, 0.1, 1.0]:
         logreg_obj = logreg_obj_wrap(DTR, LTR, l)
-        _v, _J, _d = opt.fmin_l_bfgs_b(logreg_obj, numpy.zeros(DTR.shape[0]+1), approx_grad=True) #..[0]+1 perchè 4 sono le features (w) e uno è b
+        _v, _J, _d = opt.fmin_l_bfgs_b(logreg_obj, numpy.zeros(DTR.shape[0]+1), approx_grad=True)
         _w = _v[0:DTR.shape[0]]
         _b = _v[-1]
         STE = numpy.dot(_w.T, DTE) + _b
-        LP = STE > 0 # mette true se il > è vero, altrimenti false, ovviamente '0' perchè stiamo considerando priors uguali, LP sta per labels predicted
+        LP = STE > 0
         ER = 1 - numpy.array(LP == LTE).mean()
-        print(l, round(_J, 3), str(100*round(ER, 3))+'%') #lambda, J, Error rate
+        print(l, round(_J, 3), str(100*round(ER, 3))+'%')
 
 def plot():
     plt.figure()
