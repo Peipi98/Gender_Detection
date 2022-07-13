db9679c811b50468235eba7e8be2ed4c29c70705
ALESSIO CARACHINO s296138 <alessio.carachino.g@gmail.com>
2022-07-13 23:26:51 +0200

    final, report missing

--- main.py
+++ main.py
@@ -1,207 +1,127 @@
-# from functions.calibrationFunc import calibrate_SVM
-
-'''
-+---------------------------+
-|                           |
-|         VALIDATION        |
-|                           |
-+---------------------------+
-'''
-from validation.compare_best_2 import compute_2best_plots
-from validation.validation_LR import validation_LR
-from validation.validation_weighted_LR import validation_weighted_LR
-from validation.validation_quad_LR import validation_quad_LR
-from validation.validation_MVG import validation_MVG
-from validation.validation_SVM import validation_SVM
-from validation.validation_SVM_RFB import validation_SVM_RFB
-from validation.validation_SVM_polynomial import validation_SVM_polynomial
-from validation.validation_GMM import validation_GMM_ncomp, validation_GMM_tot
-
-'''
-+---------------------------+
-|                           |
-|         EVALUATION        |        
-|                           |
-+---------------------------+
-'''
-from evaluators.evaluation_MVG import evaluation_MVG
 from evaluators.evaluation_GMM import evaluation_GMM_ncomp, experimental_GMM
 from evaluators.evaluation_LR import evaluation_LR
+from evaluators.evaluation_MVG import evaluation_MVG
 from evaluators.evaluation_SVM import evaluation_SVM
 from evaluators.evaluation_SVM_RFB import evaluation_SVM_RFB
 from evaluators.evaluation_SVM_polynomial import evaluation_SVM_polynomial
 from evaluators.evaluation_quad_LR import evaluation_quad_LR
 from evaluators.evaluation_weighted_LR import evaluation_weighted_LR
-
-#from plot_features import plot_features
-from validators import *
-import scipy.stats as stats
-
+from mlFunc import *
+from evaluators.compare_best_2 import compute_2best_plots
+from plot_features import plot_features
+from validation.validation_GMM import validation_GMM_ncomp, validation_GMM_tot
+from validation.validation_LR import validation_LR
+from validation.validation_MVG import validation_MVG
+from validation.validation_SVM import validation_SVM
+from validation.validation_SVM_RFB import validation_SVM_RFB
+from validation.validation_SVM_polynomial import validation_SVM_polynomial
+from validation.validation_compare import compare_2_validation
+from validation.validation_quad_LR import validation_quad_LR
+from validation.validation_weighted_LR import validation_weighted_LR
 
 def validation(DTR, LTR):
-    # RAW data
-    # print("############    MVG - RAW   ##############")
-    # validation_MVG(DTR, LTR, 'RAW_')
+    print("############    MVG    ##############")
+    validation_MVG(DTR, LTR, 'RAW_')
+    validation_MVG(DTR, LTR, 'GAUSSIANIZED_', Gauss_flag=True)
+    validation_MVG(DTR, LTR, DTE, LTE, 'ZNORM_', zscore=True)
 
-    #
-    # print("############    Logistic Regression - RAW    ##############")
-    # L = [1e-6, 1e-4, 1e-2, 1.0] #1e-6
-    #
-    # validation_LR(DTR, LTR, L, 'RAW_')
-    # L = [1e-4, 1e-2, 1e-1, 1.0]
-    # validation_weighted_LR(DTR, LTR, L, 'RAW_', PCA_Flag=False)
-    # validation_quad_LR(DTR, LTR, L, 'RAW_')
-    #
-    # print("############    Support Vector Machine - RAW    ##############")
-    # K_arr = [0.1, 1.0, 10.0]
-    # C_arr = [0.01, 0.1, 1.0, 10.0]
-    # K_arr = [1.0] #
-    # C_arr = [1.0] #
-    # validation_SVM(DTR, LTR, K_arr, C_arr, 'RAW_')
-    # #calibrate_SVM(DTR, LTR, 'RAW_CALIBRATED_')
-    # CON_array = [1000]
-    # K_arr = [1., 10.]
-    #
-    # validation_SVM_polynomial(DTR, LTR, K_arr, 1.0, 'RAW_', CON_array, False)
-    # K_arr = [0.1, 1., 10.]
-    # C_arr = [1., 10.]
-    # gamma_Arr = [0.001]
-    # validation_SVM_RFB(DTR, LTR, K_arr, gamma_Arr, 'RAW_', PCA_Flag=False)
+    print("############    Logistic Regression    ##############")
+    L = [1e-6, 1e-4, 1e-2, 1.0]
+    validation_LR(DTR, LTR, L, 'RAW_', PCA_Flag=True, gauss_Flag=False, zscore_Flag=False)
+    validation_LR(DTR, LTR, L, 'GAUSSIANIZED_', PCA_Flag=True, gauss_Flag=True, zscore_Flag=False)
+    validation_LR(DTR, LTR, L, 'ZNORMALIZED_', PCA_Flag=True, gauss_Flag=False, zscore_Flag=True)
+    print("############    Weighted Logistic Regression    ##############")
+    L = [1e-4, 1e-2, 1e-1, 1.0]
+    validation_weighted_LR(DTR, LTR, L, 'RAW_', PCA_Flag=True, gauss_Flag=False, zscore_Flag=False)
+    validation_weighted_LR(DTR, LTR, L, 'GAUSSIANIZED_', PCA_Flag=True, gauss_Flag=True, zscore_Flag=False)
+    validation_weighted_LR(DTR, LTR, L, 'ZNORMALIZED_', PCA_Flag=True, gauss_Flag=False, zscore_Flag=True)
 
-    print("############    Gaussian Mixture Model - RAW & gaussianization    ##############")
+    print("############    Quadratic Logistic Regression    ##############")
+    validation_quad_LR(DTR, LTR, L, 'RAW_', PCA_Flag=True, gauss_Flag=False, zscore_Flag=False)
 
-    # validation_GMM_tot(DTR, LTR, 0.5)
-    # validation_GMM_ncomp(DTR, LTR, 0.5, 2)
-    # validation_GMM_ncomp(DTR, LTR, 0.1, 2)
-    # validation_GMM_ncomp(DTR, LTR, 0.9, 2)
-
-    # Gaussianization
-
-    #    plot_features(DTR, LTR, 'GAUSSIANIZED_')
-    # print("############    MVG - gaussianization    ##############")
-
-    # validation_MVG(DTR, LTR, 'GAUSSIANIZED_', Gauss_flag=True)
-    #
-    # print("############    Logistic Regression - gaussianization    ##############")
-    # L = [1e-6, 1e-4, 1e-2]
-    # validation_LR(DTR, LTR, L, 'GAUSSIANIZED_')
-    #
-    # print("############    Support Vector Machine - gaussianization    ##############")
-    # K_arr = [0.1, 1.0, 10.0]
-    # C_arr = [0.01, 0.1, 1.0, 10.0]
-    # validation_SVM(DTR, LTR, K_arr, C_arr, 'GAUSSIANIZED_')
+    print("############    Support Vector Machine - Primal    ##############")
+    K_arr = [0.1, 1.0, 10.0]
+    C_arr = [0.01, 0.1, 1.0, 10.0]
+    validation_SVM(DTR, LTR, K_arr, C_arr, 'RAW_', gauss_Flag=False, zscore_Flag=False)
+    validation_SVM(DTR, LTR, K_arr, C_arr, 'GAUSSIANIZED_', gauss_Flag=True, zscore_Flag=False)
+    validation_SVM(DTR, LTR, K_arr, C_arr, 'ZNORMALIZED_', gauss_Flag=False, zscore_Flag=True)
 
 
-#    plot_features(DTR, LTR, 'ZNORM')
+    print("############    Support Vector Machine - Dual - Polynomial    ##############")
+    K_arr = [1., 10.]
+    validation_SVM_polynomial(DTR, LTR, [1.0, 10.0], 1.0, 'RAW_', [1000], PCA_Flag=False, gauss_Flag=False, zscore_Flag=False)
 
-    # print("############    MVG - Z Normalization    ##############")
-    # validation_MVG(DTR, LTR, DTE, LTE, 'ZNORM_', zscore=True)
-    #
-    # print("############    Logistic Regression - Z Normalization    ##############")
-    # L = [1e-6, 1e-4, 1e-2]
-    # validation_LR(DTR, LTR, L, 'ZNORM_')
-    # L = [1e-4, 1e-2, 1e-1, 1.0]
-    # validation_weighted_LR(DTR, LTR, L, 'RAW_', PCA_Flag=False)
-    #
-    # print("############    Support Vector Machine - Z Normalization    ##############")
-    # K_arr = [0.1, 1.0, 10.0]
-    # C_arr = [0.01, 0.1, 1.0, 10.0]
-    # validation_SVM(DTR, LTR, K_arr, C_arr, 'ZNORM')
+    K_arr = [0.1, 1., 10.]
+    C_arr = [1., 10.]
+
+    print("############    Support Vector Machine - Dual - RFB    ##############")
+    validation_SVM_RFB(DTR, LTR, K_arr, [0.001], 'RAW_', PCA_Flag=False, gauss_Flag=False, zscore_Flag=False)
+
+    print("############    Gaussian Mixture Model   ##############")
+
+    validation_GMM_tot(DTR, LTR, 0.5)
+    validation_GMM_ncomp(DTR, LTR, 0.5, 2)
+    validation_GMM_ncomp(DTR, LTR, 0.1, 2)
+    validation_GMM_ncomp(DTR, LTR, 0.9, 2)
 
 def evaluation(DTR, LTR, DTE, LTE):
-    # print("############    MVG - RAW   ##############")
-    # evaluation_MVG(DTR, LTR, DTE, LTE, 'RAW_')
-    #
-    # print("############    Logistic Regression - RAW    ##############")
-    # L = [1e-6]  # 1e-6
-    #
-    # validation_LR(DTR, LTR, L, 'RAW_')
-    # evaluation_LR(DTR, LTR, DTE, LTE, L, 'RAW_', PCA_Flag=False)
-    # L = [1e-4]
-    # validation_weighted_LR(DTR, LTR, L, 'RAW_', PCA_Flag=False)
-    # validation_quad_LR(DTR, LTR, L, 'RAW_')
-    #
-    # print("############    Support Vector Machine - RAW    ##############")
-    # K_arr = [1.0]
-    # C_arr = [1.0]
-    # validation_SVM(DTR, LTR, K_arr, C_arr, 'RAW_')
-    # # calibrate_SVM(DTR, LTR, 'RAW_CALIBRATED_')
-    # CON_array = [1000]
-    # K_arr = [1.]
-    #
-    # validation_SVM_polynomial(DTR, LTR, K_arr, 1.0, 'RAW_', CON_array, False)
-    # K_arr = [1.]
-    # C_arr = [10.]
-    # gamma_Arr = [0.001]
-    # validation_SVM_RFB(DTR, LTR, K_arr, gamma_Arr, 'RAW_', PCA_Flag=False)
+    DTR_GAUSS = gaussianize_features(DTR, DTR)
+    DTE_GAUSS = gaussianize_features(DTR, DTE)
+    DTR_ZNORM, DTE_ZNORM = znorm(DTR, DTE)
 
-    # print("############    Gaussian Mixture Model - RAW    ##############")
-    # evaluation_GMM_ncomp("RAW", DTR, LTR, DTE, LTE, 0.5, 2)
-    # evaluation_GMM_ncomp("RAW", DTR, LTR, DTE, LTE, 0.1, 2)
-    # evaluation_GMM_ncomp("RAW", DTR, LTR, DTE, LTE, 0.9, 2)
+    print("############    MVG   ##############")
+    evaluation_MVG(DTR, LTR, DTE, LTE, 'RAW_')
+    evaluation_MVG(DTR_GAUSS, LTR, DTE_GAUSS, LTE, 'GAUSSIANIZED_')
+    evaluation_MVG(DTR_ZNORM, LTR, DTE_ZNORM, LTE, 'Z-NORM')
 
-    # Gaussianization
+    print("############    Logistic Regression    ##############")
+    evaluation_LR(DTR, LTR, DTE, LTE, [1e-6], 'EVAL_LR_', PCA_Flag=False)
 
-    DTR_gauss = gaussianize_features(DTR, DTR)
-    DTE_gauss = gaussianize_features(DTR, DTE)
-    #    plot_features(DTR, LTR, 'GAUSSIANIZED_')
+    evaluation_LR(DTR_GAUSS, LTR, DTE_GAUSS, LTE, [1e-6], 'EVAL_LR_GAUSS_', PCA_Flag=False)
+    evaluation_LR(DTR_ZNORM, LTR, DTE_ZNORM, LTE, [1e-6], 'EVAL_LR_ZNORM_', PCA_Flag=False)
+    print("############    Weighted Logistic Regression    ##############")
+    evaluation_weighted_LR(DTR, LTR, DTE, LTE, [1e-4], 'EVAL_WEIGHTED_LR_', PCA_Flag=False)
 
-    print("############    MVG - gaussianization    ##############")
-    evaluation_MVG(DTR_gauss, LTR, DTE_gauss, LTE, 'GAUSSIANIZED_')
-    #
-    # print("############    Logistic Regression - gaussianization    ##############")
-    # L = [1e-6, 1e-4, 1e-2]
-    # validation_LR(DTR, LTR, L, evaluation_MVG(DTR, LTR, DTE, LTE, 'RAW_'))
-    #
-    # print("############    Support Vector Machine - gaussianization    ##############")
-    # K_arr = [0.1, 1.0, 10.0]
-    # C_arr = [0.01, 0.1, 1.0, 10.0]
-    # validation_SVM(DTR, LTR, K_arr, C_arr, 'GAUSSIANIZED_')
+    print("############    Quadratic Logistic Regression    ##############")
+    evaluation_quad_LR(DTR, LTR, DTE, LTE, [1e-4], 'EVAL_WEIGHTED_LR_', PCA_Flag=False)
 
-    print("############    Gaussian Mixture Model - gaussianization    ##############")
-    evaluation_GMM_ncomp("gauss.", DTR_gauss, LTR, DTE_gauss, LTE, 0.5, 2)
-    evaluation_GMM_ncomp("gauss.", DTR_gauss, LTR, DTE_gauss, LTE, 0.1, 2)
-    evaluation_GMM_ncomp("gauss.", DTR_gauss, LTR, DTE_gauss, LTE, 0.9, 2)
+    print("############    Support Vector Machine - Primal    ##############")
+    evaluation_SVM(DTR, LTR, DTE, LTE, [1.0], [1.0], 'EVAL_SVM_LR', PCA_Flag=False)
 
-    # DTR_z = stats.zscore(DTR, axis=1)
-    # DTR_z, DTE_z = znorm(DTR, DTE)
-    #    plot_features(DTR, LTR, 'ZNORM')
+    print("############    Support Vector Machine - Dual - Polynomial    ##############")
+    evaluation_SVM_polynomial(DTR, LTR, DTE, LTE, [1.0], 1.0, 'EVAL_SVM_POLY', [1000], PCA_Flag=False)
 
-    # print("############    MVG - Z Normalization    ##############")
-    # evaluation_MVG(DTR_z, LTR, DTE_z, LTE, 'Z-NORM')
+    print("############    Support Vector Machine - Dual - RFB    ##############")
+    evaluation_SVM_RFB(DTR, LTR, DTE, LTE, [1.0], [0.001], 'EVAL_SVM_RFB', PCA_Flag=False)
 
-    # print("############    Logistic Regression - Z Normalization    ##############")
-    # L = [1e-6, 1e-4, 1e-2]
-    # validation_LR(DTR, LTR, L, 'ZNORM_')
-    # L = [1e-4, 1e-2, 1e-1, 1.0]
-    # validation_weighted_LR(DTR, LTR, L, 'RAW_', PCA_Flag=False)
-    #
-    # print("############    Support Vector Machine - Z Normalization    ##############")
-    # K_arr = [0.1, 1.0, 10.0]
-    # C_arr = [0.01, 0.1, 1.0, 10.0]
-    # validation_SVM(DTR, LTR, K_arr, C_arr, 'ZNORM')
-    # print("############    Gaussian Mixture Model - gaussianization    ##############")
-    # evaluation_GMM_ncomp("Z-NORM", DTR_z, LTR, DTE_z, LTE, 0.5, 2)
-    # evaluation_GMM_ncomp("Z-NORM", DTR_z, LTR, DTE_z, LTE, 0.1, 2)
-    # evaluation_GMM_ncomp("Z-NORM", DTR_z, LTR, DTE_z, LTE, 0.9, 2)
+    print("############    Gaussian Mixture Model - RAW    ##############")
+    evaluation_GMM_ncomp("RAW", DTR, LTR, DTE, LTE, 0.5, 2)
+    evaluation_GMM_ncomp("RAW", DTR, LTR, DTE, LTE, 0.1, 2)
+    evaluation_GMM_ncomp("RAW", DTR, LTR, DTE, LTE, 0.9, 2)
+    evaluation_GMM_ncomp("gauss.", DTR_GAUSS, LTR, DTE_GAUSS, LTE, 0.5, 2)
+    evaluation_GMM_ncomp("gauss.", DTR_GAUSS, LTR, DTE_GAUSS, LTE, 0.1, 2)
+    evaluation_GMM_ncomp("gauss.", DTR_GAUSS, LTR, DTE_GAUSS, LTE, 0.9, 2)
 
 if __name__ == "__main__":
     DTR, LTR = load("Train.txt")
     DTR, LTR = randomize(DTR, LTR)
     DTE, LTE = load("Test.txt")
     DTE, LTE = randomize(DTE, LTE)
-    #plot_features(DTR, LTR, 'RAW_')
+    DTR_GAUSS = gaussianize_features(DTR, DTR)
+    plot_features(DTR, LTR, 'RAW_')
+    plot_features(DTR, LTR, 'GAUSSIANIZED_')
+    print("############    Validation    ##############")
+    validation(DTR, LTR)
+    print("############    Evaluation    ##############")
+    evaluation(DTR, LTR, DTE, LTE)
 
-    # validation(DTR, LTR)
-    '''
-    +---------------------------+
-    |                           |
-    |         EVALUATION        |        
-    |                           |
-    +---------------------------+
-    '''
 
-    #evaluation(DTR, LTR, DTE, LTE)
-
-    experimental_GMM(DTR, LTR, DTE, LTE)
-    #compute_2best_plots(DTR,LTR,DTE,LTE)
+    # Warning # Warning # Warning # Warning
+    # Warning # Warning # Warning # Warning
+    # Warning # Warning # Warning # Warning
+    # Warning: the following code has not been cleaned yet. It has been used to generate comparison plots.
+    # Creates barcharts for GMM with validation and evaluation
+    # experimental_GMM(DTR, LTR, DTE, LTE)
+    # Creates bayes error and ROC plots for 2 best models chosen (see inside)
+    # compute_2best_plots(DTR, LTR, DTE, LTE)
+    # compare_2_validation(DTR, LTR, [1e-4])
